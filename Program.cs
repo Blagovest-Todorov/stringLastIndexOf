f386d8bb316bf37483b8ce00961a8aff880b0f3b
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-19 19:04:08 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.lastIndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "gosho aaac gosho pesho ivabn pesho gosho";
+            int found = text.LastIndexOf("pesho");
+            Console.WriteLine(found);
+        }
+    }
+}
