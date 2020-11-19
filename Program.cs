99f9eb1e1ea6a72a69ae8821067a03245a55e4a2
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-19 19:07:09 +0200

    added some explanations here

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace _03.lastIndexOf
         static void Main(string[] args)
         {
             string text = "gosho aaac gosho pesho ivabn pesho gosho";
-            int found = text.LastIndexOf("pesho");
-            Console.WriteLine(found);
+            int found = text.LastIndexOf("pesho");//we search in strin text the last indexValue of occurance of pesho
+            Console.WriteLine(found); //searches in tetx string the last indexValue where str pesho met for last time
         }
     }
 }
